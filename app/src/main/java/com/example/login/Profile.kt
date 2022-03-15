60a7dc3782ba376202f8abead03cafa401f42420
Chinhalex <chinhvv1010@gmail.com>
2022-03-15 16:16:15 +0700

    login

---
+++ app/src/main/java/com/example/login/Profile.kt
@@ -0,0 +1,12 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.login.R
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
--- app/src/main/java/com/example/login/SignUp.kt
+++ app/src/main/java/com/example/login/SignUp.kt
@@ -1,32 +1,84 @@
 package com.example.login
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
+import android.widget.EditText
 import android.widget.TextView
+import android.os.Bundle
+import com.example.login.R
+import android.content.Intent
+import android.view.View
+import com.example.login.Login
+import com.example.login.SignUp
+import android.widget.Toast
 
-class SignUp : AppCompatActivity() {
-    var txtLogin: TextView? = null
-    lateinit var handler: Handler
+class SignUp : AppCompatActivity(), View.OnClickListener {
+    private var edtFullName: EditText? = null
+    private var edtEmail: EditText? = null
+    private var edtPassWord: EditText? = null
+    private var login: TextView? = null
+    private var txtLogin: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-
-        handler = Handler()
-        handler.postDelayed({
-            val intent = Intent(this, Verification_Code::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        edtFullName = findViewById<View>(R.id.edtFullName) as EditText
+        edtEmail = findViewById<View>(R.id.edtEmail) as EditText
+        edtPassWord = findViewById<View>(R.id.edtPassWord) as EditText
+        login = findViewById<View>(R.id.login) as TextView
+        login!!.setOnClickListener(this)
+        txtLogin = findViewById<View>(R.id.txtLogin) as TextView
+        txtLogin!!.setOnClickListener(this)
     }
 
-//    private fun initView() {
-//        txtLogin = findViewById(R.id.txtLogin)
-//        txtLogin?.setOnClickListener {
-//            val intent = Intent(applicationContext, Login::class.java)
-//            startActivity(intent)
-//            finish()
-//        }
-//    }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.txtLogin -> {
+                val intent2 = Intent(this@SignUp, Login::class.java)
+                intent2.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent2)
+                finish()
+            }
+            R.id.login -> registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val name = edtFullName!!.text.toString().trim { it <= ' ' }
+        val email = edtEmail!!.text.toString().trim { it <= ' ' }
+        val pass = edtPassWord!!.text.toString().trim { it <= ' ' }
+        if (name.isEmpty()) {
+            edtFullName!!.error = "Name is reqired!"
+            edtFullName!!.requestFocus()
+            return
+        }
+        if (email.isEmpty()) {
+            edtEmail!!.error = "Email is reqired!"
+            edtEmail!!.requestFocus()
+            return
+        }
+        if (pass.isEmpty()) {
+            edtPassWord!!.error = "Pass is reqired!"
+            edtPassWord!!.requestFocus()
+            return
+        }
+        if (email != EMAIL) {
+            Toast.makeText(this@SignUp, "Email is invalid", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (pass != PASS) {
+            Toast.makeText(this@SignUp, "pass is invalid", Toast.LENGTH_LONG).show()
+            return
+        } else {
+            val intent = Intent(this@SignUp, Login::class.java)
+            intent.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    companion object {
+        private const val EMAIL = "ronaldo@gmail.com"
+        private const val PASS = "123456"
+    }
 }
